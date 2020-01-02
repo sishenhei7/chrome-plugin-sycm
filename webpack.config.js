a5a59c4c0d1615a10c7f92d83b2a6f9f9e9ff2e6
sishenhei7 <982505136@qq.com>
2020-01-02 16:47:38 +0800

    feat: 增加source-map便于调试

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        popup: './js/popup.js',
-        background: './js/background.js',
-        contentScript: './js/contentScript.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: path.join(__dirname, ""),
-        publicPath: '/dist/',
-        watchContentBase: true,
-        compress: true,
-        port: 3009,
-        open: true,
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                exclude: /node_modules/,
-                options: {
-                    configFile: path.resolve(__dirname, './.eslintrc'),
-                },
-                loader: "eslint-loader"
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ],
-    },
+  mode: process.env.NODE_ENV,
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : 'none',
+  entry: {
+    popup: './js/popup.js',
+    background: './js/background.js',
+    contentScript: './js/contentScript.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, ""),
+    publicPath: '/dist/',
+    watchContentBase: true,
+    compress: true,
+    port: 3009,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        options: {
+            configFile: path.resolve(__dirname, './.eslintrc'),
+        },
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ],
+  },
 };
